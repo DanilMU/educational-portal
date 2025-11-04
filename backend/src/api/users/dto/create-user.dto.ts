38e4d7ebdefe424ab201f6eab244c3d0607702f3
DanilMU <danilmusatov3@yandex.ru>
2025-11-04 22:59:25 +0300

    feat(users): add CreateUserDto and UpdateUserDto for admin operations

---
+++ backend/src/api/users/dto/create-user.dto.ts
@@ -0,0 +1,31 @@
+import { Role } from '@prisma/client';
+import {
+	IsEmail,
+	IsEnum,
+	IsOptional,
+	IsString,
+	MinLength
+} from 'class-validator';
+
+export class CreateUserDto {
+	@IsEmail()
+	public email: string;
+
+	@MinLength(8, {
+		message: 'Password must be at least 8 characters long'
+	})
+	@IsString()
+	public password: string;
+
+	@IsString()
+	@IsOptional()
+	public firstName?: string;
+
+	@IsString()
+	@IsOptional()
+	public lastName?: string;
+
+	@IsEnum(Role)
+	@IsOptional()
+	public role?: Role;
+}
